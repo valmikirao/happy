a2a3c899c3f1a54e4ca41a12b6153c2ce8aa967f
valmikirao <valmikirao@yahoo.com>
2017-08-27 20:59:27 -0400

    Remove dist from git

--- dist/server-lib/isomporphic-types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/// <reference path="types.d.ts"/>
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-;
-;
-//# sourceMappingURL=isomporphic-types.js.map
--- dist/server-lib/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-mongoose.Promise = Promise;
-var foo = 5;
-//# sourceMappingURL=test.js.map
